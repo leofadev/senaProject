edaeb2fe745c210f2d954725c8e94586ff96403a
leonardo <pintoleonardo1023@gmail.com>
2023-06-06 16:12:50 -0500

    feat: :sparkles: se implementó Laravel-Admin-LTE y se modificaron algunos archivos
    se modifico: modified:   app/Providers/RouteServiceProvider.php modified:   composer.json modified:   composer.lock modified:   routes/web.php   se agregaron: config/adminlte.php lang/ public/vendor/ resources/views/dash/
    
    senaProject

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dash', function () {
+    return view('dash.index');
+})->middleware(['auth', 'verified'])->name('dash');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
